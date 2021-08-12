a1699643ac337752bd50fccdd3f88e8b1eea76d5
hwm <1098155959@qq.com>
2021-08-12 11:39:41 +0800

    第二个用户修改了HelloWord

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,15 +6,5 @@ public class HelloWorld {
 		System.out.println("Hello World2");
 		System.out.println("3");
 		System.out.println("Hello World3");
-		System.out.println("4");
-		System.out.println("Hello World4");
-		System.out.println("5");
-		System.out.println("Hello World5");
-		System.out.println("6");
-		System.out.println("Hello World6");
-		System.out.println("7");
-		System.out.println("Hello World7");
-		System.out.println("8");
-		System.out.println("Hello World8");
 	}
 }
