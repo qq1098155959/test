ddf07a9b2a566c99d28e4dc381922c31952ee725
hwm <1098155959@qq.com>
2021-08-12 10:52:31 +0800

    新增了

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,10 @@
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("1");
-		System.out.println("Hello World");
+		System.out.println("Hello World1");
 		System.out.println("2");
-		System.out.println("Hello World");
+		System.out.println("Hello World2");
+		System.out.println("3");
+		System.out.println("Hello World3");
 	}
 }
