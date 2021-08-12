0c30de04551514f0db0cdf2ad9c40ce5f54e0515
hwm <1098155959@qq.com>
2021-08-12 17:54:55 +0800

    用户3修改了HelloWOrd

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,14 +9,6 @@ public class HelloWorld {
 		System.out.println("4");
 		System.out.println("Hello World4");
 		System.out.println("5");
-		System.out.println("Hello World5");
-		System.out.println("6");
-		System.out.println("Hello World6");
-		System.out.println("7");
-		System.out.println("Hello World7");
-		System.out.println("8");
-		System.out.println("Hello World8");
-		System.out.println("9");
-		System.out.println("Hello World9");
+		System.out.println("Hello World555555");
 	}
 }
