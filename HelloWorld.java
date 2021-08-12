06c40d7fa768f25c54c9149babdcf191c19468e7
hwm <1098155959@qq.com>
2021-08-12 10:33:08 +0800

    修改了哪哪哪的内容。哪部分功能

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 		System.out.println("1");
 		System.out.println("Hello World");
 		System.out.println("2");
-		System.out.println("Hello World22222哈哈哈哈哈哈");
+		System.out.println("Hello World22222哈哈");
 		System.out.println("3");
 		System.out.println("Hello World3333");
 	}
