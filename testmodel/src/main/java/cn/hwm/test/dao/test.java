43dccef8cdf77d835b3dba74df5fb3140bf9197b
hwm <1098155959@qq.com>
2021-08-16 17:45:58 +0800

    黄伟沐新建了testmodel

---
+++ testmodel/src/main/java/cn/hwm/test/dao/test.java
@@ -0,0 +1,26 @@
+package cn.hwm.test.dao;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class test {
+    public static void main(String[] args) {
+        List<User> userList = new ArrayList<>();
+        //提取用户名
+        Collection<String> transform = Collections2.transform(userList, new Function<User, String>() {
+            @Nullable
+            @Override
+            public String apply(@Nullable User input) {
+                return input.getName();
+            }
+        });
+        //取List中单一数据存放在新的List当中
+        List<String> list = userList.stream().map(User::getName).collect(Collectors.toList());
+    }
+}
