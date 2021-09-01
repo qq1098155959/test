b2c975f1ad1d44abd208c891b32bfe62ed6b1fdd
hwm <1098155959@qq.com>
2021-09-01 13:59:14 +0800

    提交

--- testmodel/src/main/java/cn/hwm/test/dao/test.java
+++ testmodel/src/main/java/cn/hwm/test/dao/test.java
@@ -22,6 +22,17 @@ public class test {
         //取List中单一数据存放在新的List当中
         List<String> list = userList.stream().map(User::getName).collect(Collectors.toList());
 
+
+        //将查询内容取出单独Id存放在一个List中，再根据List去查询对象中所有数据。再把返回的List对象封装在Map中，for循环中需要取数据就根据Key在map中取
+//        List<Integer> communityIdlist = hotPage.getResult().stream().map(AppCommunityHot::getCommunityId).collect(Collectors.toList());
+//        List<AppCommunity> appCommunityList = communityDao.getAppCommunityListByIds(communityIdlist);
+//        if (CollectionUtils.isEmpty(appCommunityList)){
+//            resp.setCode(YYErrorCode.FAIL);
+//            resp.setMessage("社区列表为空");
+//            return resp;
+//        }
+//        Map<Integer, AppCommunity> communityMap = appCommunityList.stream().collect(Collectors.toMap(AppCommunity::getId, Function.identity(), (k1, k2) -> k1));
+
         //返回格式多包一层
         Map<String,List<User>> maplist = new HashMap<>();
         maplist.put("list",userList);
